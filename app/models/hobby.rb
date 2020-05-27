c0a9b8b328d01d0d613684a35a80d7e63bf78f49
sap2411 <steven.parsons.us@gmail.com>
2020-05-26 19:45:10 -0700

    mvp 1.1

--- app/models/hobby.rb
+++ app/models/hobby.rb
@@ -3,4 +3,8 @@ class Hobby < ApplicationRecord
     has_many :users, through: :user_hobbies
     has_many :hobby_materials
     has_many :materials, through: :hobby_materials
+
+    def self.sorted_by_materials
+        self.all.sort_by {|hobby| hobby.materials.count}
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,16 @@
 class User < ApplicationRecord
     has_many :user_hobbies
     has_many :hobbies, through: :user_hobbies
+
+    def simplest_hobby
+        self.sorted.first
+    end
+
+    def most_complex_hobbie
+        self.sorted.last
+    end
+
+    def sorted
+        self.hobbies.sort_by {|hobby| hobby.materials.count}
+    end
 end
