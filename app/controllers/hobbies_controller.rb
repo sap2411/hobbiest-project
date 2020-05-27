9a087087ce7d71106819f33f4d5acaba21bb24e2
github username <github email address>
2020-05-26 23:22:15 -0700

    working on creating hobby new form

--- app/controllers/hobbies_controller.rb
+++ app/controllers/hobbies_controller.rb
@@ -1,5 +1,9 @@
 class HobbiesController < ApplicationController
     before_action :require_login
+
+    # So far I can only get updates to this array to persist through one reload of new.erb DK
+    @@categories = ["Test One", "Test Two", "Test Three"]
+
     def index
         @hobbies = Hobby.all
     end
@@ -15,4 +19,25 @@ class HobbiesController < ApplicationController
         redirect_to @hobby
     end
     
+    def new
+        @categories = @@categories
+        @hobby = Hobby.new
+    end
+
+    def create
+        params[:hobby][:username] = session[:username]
+        if !@@categories.include? params[:hobby][:category]
+            @@categories << params[:hobby][:category]
+        end
+        @hobby = Hobby.create(hobby_params)
+        @hobby.materials.create(name: params[:materials][:name], url:"https://www.amazon.com/s?k=#{params[:materials][:name]}&ref=nb_sb_noss_2")
+        byebug
+        redirect_to @hobby
+    end
+
+    private
+
+    def hobby_params
+        params.require(:hobby).permit(:name, :category, :description, :created_by, materials: [:name])
+    end
 end
