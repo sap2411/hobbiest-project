8acc095302721a87a849709265a9f8dc72eb90f9
sap2411 <steven.parsons.us@gmail.com>
2020-05-28 09:25:17 -0700

    get delete

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   post '/logout' => 'sessions#destroy'
+  get '/logout' => 'sessions#destroy'
   get '/signup' => 'users#new'
   post '/signup' => 'users#create'
   get '/analytics' => 'welcome#analytics'
